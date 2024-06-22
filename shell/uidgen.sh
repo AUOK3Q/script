04f6933e274435c3545c5b397f1298235e2e82b1
AUOK3Q <1647552462@qq.com>
2024-06-22 11:10:46 +0800

    'first'

---
+++ shell/uidgen.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##  随机生成1000 ~ 60000随机数
+function mimvp_uid_num() {
+  min=$1
+  max=$2
+  mid=$(($max-$min+1))
+  num=$(head -n 20 /dev/urandom | cksum | cut -f1 -d ' ')
+  randnum=$(($num%$mid+$min))    
+ 
+  # 排除1000, 60000可以任意添加
+  port_exclude='1000,60000'
+  flag=`echo ${port_exclude} | grep ${randnum} | wc -l`
+  while [ "$flag" -eq "1" ]
+  do
+    num=$(head -n 20 /dev/urandom | cksum | cut -f1 -d ' ')
+    randnum=$(($num%$mid+$min))    
+    flag=`echo ${port_exclude} | grep ${randnum} | wc -l`
+  done
+  echo $randnum
+}
+ 
+function print_uid_num() {
+  for i in {1..1};
+  do
+    randnum=$(mimvp_uid_num 1000 60000)
+    echo -e $randnum
+  done
+}
+ 
+print_uid_num
